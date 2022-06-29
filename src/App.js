f796598bfc99063c0aee5d4679062c5796c5dff8
Parshin D.V <parshin_dv@atommash.ru>
2022-06-29 16:26:24 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="img/logo.png" alt="Logotype" />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">Система 5С на производстве</h3>
+            <p className="opacity-5">поиск выявленных нарушений</p>
+          </div>
+        </div>
       </header>
+
+      <div className="content p-40">
+        <h1>Все нарушения</h1>
+
+        <div className="card"></div>
+
+        {
+          <div className="card">
+            BasicTable
+            {/*             <img src="" alt=""></img>
+            <p></p>
+            <div>
+              <div>
+                <span>Участок:</span>
+                <b>Таб.номер</b>
+              </div>
+              <button>
+                <img src="" alt=""></img>
+              </button>
+            </div> */}
+          </div>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
